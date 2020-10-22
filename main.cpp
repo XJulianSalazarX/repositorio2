a3aa5272aab629f2eb7faefe3691a0fb984d06e0
julian salazar <julian.salazard@udea.edu.co>
2020-10-22 09:09:19 -0500

    rama develop

--- main.cpp
+++ main.cpp
@@ -8,8 +8,14 @@ using namespace std;
 int main()
 {
     char a[TAM] = "AB11CD44";
-    for(int i=0; i<TAM; i++){
+    for(int i = 0; i < TAM; i++){
         cout << a[i] << endl;
+    }
+    cout << "Imprimir los numeros de la cadena original" << endl;
+    for(int i = 0; i < TAM; i++){
+        if(a[i]>='0' and a[i]<='9'){
+            cout << a[i] << endl;
+        }
     }
     return 0;
 }
